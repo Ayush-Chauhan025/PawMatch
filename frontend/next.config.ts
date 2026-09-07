dc567135346fbae9fa0dfcd6d147352556857dbb
Ayush Chauhan <ayushchauhan975820@gmail.com>
2026-09-07 23:20:32 +0530

    add config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // Allows uploads up to 10MB
+    },
+  },
 };
 
 export default nextConfig;
