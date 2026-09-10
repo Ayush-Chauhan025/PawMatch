4125a0fd3cb4a8dac9b715d925cef23a1ac73dee
Ayush Chauhan <ayushchauhan975820@gmail.com>
2026-09-11 01:15:38 +0530

    Dependency

--- frontend/app/spotted_pet/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Spotted_Pet_Page(){
-    return <div>
-        Spotted Pet
-        <div className="bg-white">
-            <input type="Image" />
-        </div>
-    </div>
-}
